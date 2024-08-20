e519d856cc562fe7b0f10908d6432ed69dbbd316
Atul Yadav <atuly3051@gmail.comm>
2024-08-20 19:15:01 +0530

    Changed card

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     
       <form onSubmit={props.handleClick}>
       <input onChange={props.func} name= "title" placeholder='Title' value={props.value_in}></input>
-      <textarea onChange={props.func} id = "content" name="content"placeholder='Take a Note....'  value={props.value_text}></textarea>
+      <textarea onChange={props.func} id = "content" name="content"placeholder='Take a Note...'  value={props.value_text}></textarea>
       <button>ADD</button>
       </form>
    
