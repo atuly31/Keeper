96f683f46d9845aa06529896b8a5aaba8bb546ea
Atul Yadav <atuly3051@gmail.comm>
2024-08-21 18:16:21 +0530

    Customised Button

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import { FileAddOutlined} from '@ant-design/icons';
 const Card = (props) => {
   return (
     
       <form onSubmit={props.handleClick}>
       <input onChange={props.func} name= "title" placeholder='Title' value={props.value_in}></input>
       <textarea onChange={props.func} id = "content" name="content"placeholder='Take a Note...'  value={props.value_text}></textarea>
-      <button>ADD</button>
+      <button><FileAddOutlined /></button>
       </form>
    
     
--- src/Component/Notes.js
+++ src/Component/Notes.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import { DeleteFilled} from '@ant-design/icons';
 const Container = (props) => {
   return (
     <div className='note'>
        <h1>{props.title}</h1>
        <p>{props.content}</p>
-       <button onClick={()=>{props.handle_Button(props.idx)}}>Delete</button>
+       <button onClick={()=>{props.handle_Button(props.idx)}}><DeleteFilled /></button>
     </div>
   )
 }
